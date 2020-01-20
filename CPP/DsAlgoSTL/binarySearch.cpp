5b41ebb25f162549c2149c977437e4d64b440aeb
SinghHrmn <singh.hrmn98@gmail.com>
2020-01-20 17:45:24 +0530

    BS updated

--- CPP/DsAlgoSTL/binarySearch.cpp
+++ CPP/DsAlgoSTL/binarySearch.cpp
@@ -1,1 +1,26 @@
-// code is yet to be include
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/*
+
+Binary search code is implemented in STL library under algorithm
+
+*/
+
+int main()
+{
+    // create an array
+    int arr[10] = {1, 3, 4, 5, 6, 8, 19, 20};
+
+    //binary search requires 3 parameters
+    //forward iterator --> start, forward iterator --> end, const value --> Key
+    //fourth optional parameter for custom comparator function
+    //binary_search(start, end, key, comparator) | returns bool
+    if (binary_search(arr, arr + 4, 5))
+    {
+        cout << "5 is present";
+    }
+
+    return 0;
+}
--- CPP/DsAlgoSTL/sort.cpp
+++
@@ -1,1 +0,0 @@
-//code is yet to be included
