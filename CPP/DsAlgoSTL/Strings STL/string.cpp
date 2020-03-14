9616abee4097a5a152fac6ec7e6ac33f1e816399
Harmandeep Singh <singh.hrmn98@gmail.com>
2020-03-14 11:26:33 +0530

    string modifies

--- CPP/DsAlgoSTL/Strings STL/string.cpp
+++ CPP/DsAlgoSTL/Strings STL/string.cpp
@@ -9,9 +9,15 @@ Headers to include:
 <cstring>
 
 Methods discussed are :
-.length() | returns the length of string
-getline() | taking input from user
-sort()    | for sorting strings lexiographically
+.length()    | returns the length of string
+.compare(s1) | compare the string s1 and returns 0 if strings are equal
+.clear()     | for clearing the string 
+.empty()     | returns a boolean value True or False
+.find(s1)    | returns the index of string s1 
+.erase(i, n) | removes n chars from index i of string
+
+getline()    | taking input from user
+sort()       | for sorting strings lexiographically
 */
 
 //call at Line: 62 Custom compare function
@@ -28,7 +34,12 @@ int main()
 
     //declaring string
     //method : 1
+    string tmp("temperary data");
+    char a[] = {'H', 'e', 'l', 'l', 'o', '\0'};
+    string hello(a);
     string name = "Harmandeep";
+    string strcopy(name);      // copy value of one string to another
+    string strcopy1 = strcopy; // copy value of one string to anothers
 
     //method : 2 | input from user
     string work;
@@ -43,7 +54,20 @@ int main()
     {
         cout << name[i] << " ";
     }
+    cout << endl;
 
+    // Parsing using iterators
+    for (auto it = name.begin(); it != name.end(); it++)
+    {
+        cout << (*it) << " ";
+    }
+    cout << endl;
+
+    // Parsing using For each loop
+    for (auto c : name)
+    {
+        cout << c << " ";
+    }
     cout << endl;
 
     //array of strings
@@ -72,7 +96,32 @@ int main()
 
     cout << endl;
 
+    // Append
     string s1 = "Hello ";
     string s2 = "world";
-    cout << s1 + s2;
+    cout << s1 + s2 << "\n";
+    s1.append(s2);
+
+    cout << s1 << "\n";
+
+    // Removing elements
+    s1.clear();
+
+    // Finding if the string is empty
+    string empstr;
+    if (empstr.empty())
+    {
+        cout << "String is empty \n";
+    }
+
+    // Finding a word in string
+    string phrase = "I love to eat apple and mango";
+    int idx = phrase.find("apple");
+    cout << idx << "\n";
+
+    // Erasing a word from string;
+    string word = "apple";
+    int n = word.length();
+    phrase.erase(idx, n + 1); // +1 for removing space
+    cout << phrase << "\n";
 }
