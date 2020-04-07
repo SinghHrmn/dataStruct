9a0d0995bd5fe40ed7a7b5f2bf91b346243b5a61
Harmandeep Singh <singh.hrmn98@gmail.com>
2020-04-07 18:26:45 +0530

    vector updated

--- CPP/DataStructure/Vector/Vector.cpp
+++ CPP/DataStructure/Vector/Vector.cpp
@@ -4,19 +4,28 @@ using namespace std;
 
 int main()
 {
-    vector v;
+    vector<char> v;
 
-    v.push_back(10);
+    v.push_back(43);
 
-    v.push_back(9);
+    v.push_back(49);
 
-    v.push_back(8);
+    v.pop_back();
 
-    v.push_back(7);
+    cout << v.capacity() << endl;
 
-    v.push_back(6);
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
 
-    v.push_back(4);
+    cout << "\n";
+
+    vector<int> v;
+
+    v.push_back(43);
+
+    v.push_back(49);
 
     v.pop_back();
 
--- CPP/DataStructure/Vector/vector.h
+++ CPP/DataStructure/Vector/vector.h
@@ -1,23 +1,23 @@
-
+template <typename T>
 class vector
 {
     int cs = 0;
     int ms = 1;
-    int *arr;
+    T *arr;
 
 public:
     vector()
     {
-        arr = new int[ms];
+        arr = new T[ms];
     }
 
-    void push_back(int num)
+    void push_back(T num)
     {
         // check the ms
         if (ms == cs)
         {
-            int *old_arr = arr;
-            arr = new int[ms * 2];
+            T *old_arr = arr;
+            arr = new T[ms * 2];
             ms = ms * 2;
 
             for (int i = 0; i <= cs; i++)
@@ -32,22 +32,22 @@ public:
         cs++;
     }
 
-    int begin()
+    T begin()
     {
         return arr[0];
     }
 
-    int end()
+    T end()
     {
         return arr[cs - 1];
     }
 
-    int pop_back()
+    void pop_back()
     {
         cs--;
     }
 
-    int operator[](int i)
+    T operator[](int i)
     {
         return arr[i];
     }
