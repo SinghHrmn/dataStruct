73ddff89c706139b8a6a914a5272e542099ebb2d
Harmandeep Singh <singh.hrmn98@gmail.com>
2020-06-14 02:48:54 +0530

    Added Singly Linked List

---
+++ Python/DataStructure/SLinkedList.py
@@ -0,0 +1,16 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.nextnode = None
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
+
+a.nextnode = b
+b.nextnode = c
+c.nextnode = d
+
+a.nextnode.value
+# prints 2
